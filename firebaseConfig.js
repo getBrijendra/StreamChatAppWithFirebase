39532fad07fa7ca39d84d47dca1d2f682954f6da
Brijendra Kumar <36080430+getBrijendra@users.noreply.github.com>
2024-05-04 16:38:51 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBf6ddgVfBD-UeprIlNwCNcnuS0n4gG2Gs",
-    authDomain: "chat-app-demo-bc24b.firebaseapp.com",
+    apiKey: "AIzaSyBffwejJUJJ886NS0n4gG2Gs",
+    authDomain: "chat-app-demo-bc2f4b.firebaseapp.com",
     databaseURL: "https://chat-app-demo-bc24b-default-rtdb.firebaseio.com",
     projectId: "chat-app-demo-bc24b",
-    storageBucket: "chat-app-demo-bc24b.appspot.com",
-    messagingSenderId: "747021367728",
-    appId: "1:747021367728:web:2bd63a4a49ea25123cfe75",
-    measurementId: "G-L1CD1F9G2S"
+    storageBucket: "chat-app-demo-berc24b.appspot.com",
+    messagingSenderId: "5345634636234",
+    appId: "1:22321436774343:web:brthe34564634tl4t6dfv",
+    measurementId: "G-L1CDGGUJ342S"
 }
 
 const app = initializeApp(firebaseConfig);
